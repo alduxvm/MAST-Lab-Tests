3edb043a3a0d107850001209632accd9ad36646c
Aldo Vargas <alduxvm@gmail.com>
2014-12-01 14:40:51 +0000

    First commit
    Nothing special, just changing the repository.

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""main.py: Main module to test communication with a MultiWii Board."""
+
+__author__ = "Aldo Vargas"
+__copyright__ = "Copyright 2014 Aldux.net"
+
+__license__ = "GPL"
+__version__ = "1"
+__maintainer__ = "Aldo Vargas"
+__email__ = "alduxvm@gmail.com"
+__status__ = "Development"
+
+
+import serial, struct, time
+from multiwii import MultiWii
+
+if __name__ == "__main__":
+
+  serialPort = "/dev/tty.usbserial-A101CCVF"
+
+  board1 = MultiWii(serialPort)
+  #board2 = MultiWii("/dev/tty.usbserial-AM016WP4")
+
+  try:
+    while True:
+
+      board1.getData(MultiWii.ATTITUDE)
+      #cfg.plotData()  
+  except Exception,error:
+    print "Error: "+str(error)
